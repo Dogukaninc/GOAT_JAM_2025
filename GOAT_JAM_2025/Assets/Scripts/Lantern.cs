70908d992c29a88f26247c0c0af8b4dc70a98d24
Doğukan <d_incirbolen@hotmail.com>
2025-05-31 22:12:14 +0300

    +Update : new enemy imported

--- GOAT_JAM_2025/Assets/Scripts/Lantern.cs
+++ GOAT_JAM_2025/Assets/Scripts/Lantern.cs
@@ -1,6 +1,8 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class Lantern : MonoBehaviour
 {
@@ -8,10 +10,13 @@ public class Lantern : MonoBehaviour
     [SerializeField] private Animator lanternAnimator;
     [SerializeField] private float lanternLightCharge;
     [SerializeField] private float chargeValueMultiplier = 1f;
+    [SerializeField] private Slider lanternChargeSlider;
 
     public bool isLanternOn;
     private Collider collider;
     private Light light;
+    private float maxLanternLightCharge = 100f;
+    private Tween _sliderTween;
 
     void Awake()
     {
@@ -30,11 +35,30 @@ public class Lantern : MonoBehaviour
         {
             if (lanternLightCharge <= 0) return;
             lanternLightCharge -= Time.deltaTime * chargeValueMultiplier;
+            UpdateLanternUI();
         }
         else
         {
+            if (lanternLightCharge >= maxLanternLightCharge) return;
             lanternLightCharge += Time.deltaTime * chargeValueMultiplier;
+            UpdateLanternUI();
         }
+    }
+    
+    public void UpdateLanternUI()
+    {
+        var roundedHealth = (float)System.Math.Round(lanternLightCharge, 2);
+
+        if (lanternChargeSlider != null)
+        {
+            if (_sliderTween != null && _sliderTween.IsActive()) _sliderTween.Kill();
+            _sliderTween = DOVirtual.Float(lanternChargeSlider.value, roundedHealth / maxLanternLightCharge, 0.3f, x => { lanternChargeSlider.value = x; });
+        }
+    }
+
+    private void UpdateSlider()
+    {
+        lanternChargeSlider.value = lanternLightCharge / maxLanternLightCharge;
     }
 
     public void OnLanternOn()
